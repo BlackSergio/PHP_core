3780c67a2210fa3ffe83bd7889379cbe573867ae
SergioBlack <sergionopain@gmail.com>
2018-02-01 22:06:50 +0300

    Topic 1. PHP 7 general overview.

--- topic1/PHP7GeneralOverview.php
+++ topic1/PHP7GeneralOverview.php
@@ -1,12 +1,15 @@
 <?php
 /**
  * @link http://php.net/manual/en/migration70.new-features.php
+ * @link http://php.net/manual/en/migration71.new-features.php
+ * @link http://php.net/manual/en/migration72.new-features.php
  * Useful
  * @link https://www.phpini.in/php/php-incomplete-class-object.html
+ * @link https://habrahabr.ru/company/mailru/blog/275497/
  */
 
 /**
- * New features:
+ * New features 7.0:
  *  - Scalar type declarations
  *  - Return type declarations
  *  - Null coalescing operator
@@ -20,5 +23,38 @@
  *  - Expectations
  *  - Group use declarations
  *  - Generator Return Expressions
- * @todo Generator delegation
+ *  - Generator delegation
+ *  - Integer division with intdiv()
+ *  - Session options
+ * New features 7.1:
+ *  - Nullable types
+ *  - Void functions
+ *  - Symmetric array destructuring
+ *  - Class constant visibility
+ *  - iterable pseudo-type
+ *  - Multi catch exception handling
+ *  - Support for keys in list()
+ *  - Support for negative string offsets
+ *  - Support for AEAD in ext/openssl
+ *  - Convert callables to Closures with Closure::fromCallable()
+ *  - Asynchronous signal handling
+ *  - HTTP/2 server push support in ext/curl
+ * New feature 7.2:
+ *  - New object type
+ *  - Extension loading by name
+ *  - Abstract method overriding
+ *  - Sodium is now a core extension
+ *  - Password hashing with Argon2
+ *  - Extended string types for PDO
+ *  - Additional emulated prepares debugging information for PDO
+ *  - Support for extended operations in LDAP
+ *  - Parameter type widening
+ *  - Allow a trailing comma for grouped namespaces
+ *  - proc_nice() support on Windows
+ *  - pack() and unpack() endian support
+ *  - Enhancements to the EXIF extension
+ *  - New features in PCRE
+ *  - SQLite3 allows writing BLOBs
+ *  - Oracle OCI8 Transparent Application Failover Callbacks
+ *  - Enhancements to the ZIP extension
  */
